9e4b8848adefda9c75280677a3a8cbee88ebef71
Shriyadita <shriyaditabhilkar@gmail.com>
2023-06-12 10:24:35 -0500

    Initial Commit

---
+++ githubpractice/githubpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubpracticeApp.swift
+//  githubpractice
+//
+//  Created by Nitin Bhilkar on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct githubpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
